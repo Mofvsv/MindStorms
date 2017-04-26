bb77dece2a9fa412c39b2c1a797cfb127b88336c
MUstaf Eli <elimustaf@gmail.com>
2017-04-26 00:22:06 -0700

    make a circle out of squares

--- mindStorms.py
+++ mindStorms.py
@@ -15,13 +15,15 @@ def draw_art():
     brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
-    draw_square(brad)
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
 
     # Create the turtle Angie - Draws a circle
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
+##    angie = turtle.Turtle()
+##    angie.shape("arrow")
+##    angie.color("blue")
+##    angie.circle(100)
 
     window.exitonclick()
 draw_art()
